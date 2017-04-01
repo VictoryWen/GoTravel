b0a33a31d8b2460c2eff8441c3877fc0bb8b61e7
xuwen <superman1969@sina.com>
2017-04-01 16:12:19 +0800

    组件框架

---
+++ GoTravel/Main/Theory/Controller/MPTheoryViewController.h
@@ -0,0 +1,21 @@
+//
+//  MPTheoryViewController.h
+//  MobileProject
+//
+//  Created by wujunyang on 2017/2/16.
+//  Copyright © 2017年 wujunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+#import "MPCodeStandards.h"
+#import "MPCodeStarndardCoping.h"
+#import "MPDelegateCodeStandards.h"
+#import "MPViewControllerLifeCycle.h"
+#import "MPRunTimeViewController.h"
+#import "MPMultithreadViewController.h"
+#import "MPProtocolOptionalViewController.h"
+
+@interface MPTheoryViewController : BaseViewController<MPCodeStandardsDelegate>
+
+@end
